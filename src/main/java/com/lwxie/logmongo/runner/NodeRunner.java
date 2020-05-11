1a075d986efd135d97e89b52295d2307b8e0cc93
longwei.xie <xlw10010>
2020-05-11 10:44:37 +0800

    新增输出到kafka

--- src/main/java/com/lwxie/logmongo/runner/NodeRunner.java
+++ src/main/java/com/lwxie/logmongo/runner/NodeRunner.java
@@ -15,9 +15,15 @@ public class NodeRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("mongo-info");
+        log.info("我是来测试kafka的啦！！！");
         log.warn("mongo-warn");
         log.error("mongo-error");
         log.debug("mongo-debug");
         System.out.println("gogogo");
+//        try {
+//            int i = 1 / 0;
+//        } catch (Exception e) {
+//            log.error("错误啦。{}", getStackTraceAsString(e));
+//        }
     }
 }
